6c94c85e0b205bb7b35d2fbf1d689b58c32a63ee
Ghulam Murtaza <murtazakasur7@gmail.com>
2026-04-29 17:24:15 +0500

    Login and Signup Forms ready

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -36,6 +36,15 @@ export default function AuthPage() {
 
         {isLogin ? <LoginPage /> : <RegisterPage />}
       </div>
+
+      <p className='text-sm text-black/80 mt-5'>By signing in, you agree to our <span className='font-semibold cursor-pointer'>Terms</span> and <span className='font-semibold cursor-pointer'>Privacy Policy</span>.</p>
+
+      <div className='flex items-center justify-center gap-5 text-sm font-bold text-black/60 mt-8 mb-2'>
+        <span className='cursor-pointer'>Support</span>
+        <span className='cursor-pointer'>Trust & Safety</span>
+        <span className='cursor-pointer'>Community</span>
+      </div>
+
     </div>
   );
 }
@@ -110,59 +119,137 @@ function RegisterPage() {
   }
 
   return (
-    <div className='flex items-center justify-center w-full'>
+    <div className='flex items-center justify-center flex-col w-full min-h-[400px]'>
 
-      <form onSubmit={form} className='p-5 rounded-md flex items-center justify-between flex-col gap-5 w-[320px]'>
+      <form
+        onSubmit={form}
+        className='rounded-md flex items-start justify-start flex-col  w-full'
+      >
 
-        <h1 className='text-5xl font-medium text-center'>Register</h1>
+        <div className='mb-6'>
+          <h2 className='text-2xl font-semibold'>Welcome back</h2>
+          <p className='text-sm text-Text-Secondary'>Enter your details to access your account</p>
+        </div>
 
-        <div className='flex items-start justify-start gap-2 flex-col w-full'>
+        <div className='flex items-start justify-start gap-4 flex-col w-full'>
 
           <div className='flex flex-col gap-1 w-full'>
-            <label htmlFor="name">Name</label>
-            <div className='relative w-full'>
-              <span className='absolute top-0 left-0 -translate-y-1/2'>
-                <UserRound />
-              </span>
-              <input disabled={loading} value={username} onChange={(e) => setUsername(e.target.value)} className='border-2 px-2 py-2 rounded-md border-Input-Border focus:border-Input-Focus outline-0 w-full' type="text" id='name' placeholder='Your Name' name='username' />
+            <label
+              className='text-[13px] font-semibold text-Text-Primary'
+              htmlFor="username"
+            >Name</label>
+
+            <div className='w-full flex items-center justify-center py-2 px-4 border rounded-md border-Input-Border focus:border-Input-Focus'>
+
+              <UserRound size={20} className='text-Text-Secondary' />
+
+              <input
+                disabled={loading}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className='px-3 py-1 w-full rounded-md border-0 outline-0 text-sm'
+                type="text"
+                id='username'
+                placeholder='Jhon Doe'
+                name='username'
+              />
             </div>
+
           </div>
 
           <div className='flex flex-col gap-1 w-full'>
-            <label htmlFor="email">Email</label>
-            <input disabled={loading} value={email} onChange={(e) => setEmail(e.target.value)} className='border-2 px-2 py-1 rounded-md border-Input-Border focus:border-Input-Focus outline-0' type="email" id='email' placeholder='Your Email' name='email' />
+            <label
+              className='text-[13px] font-semibold text-Text-Primary'
+              htmlFor="email"
+            >Email Address</label>
+
+            <div className='w-full flex items-center justify-center py-2 px-4 border rounded-md border-Input-Border focus:border-Input-Focus'>
+
+              <Mail size={20} className='text-Text-Secondary' />
+
+              <input
+                disabled={loading}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className='px-3 py-1 w-full rounded-md border-0 outline-0 text-sm'
+                type="email"
+                id='email'
+                placeholder='name@example.com'
+                name='email'
+              />
+            </div>
+
           </div>
 
           <div className='flex flex-col gap-1 w-full'>
-            <label htmlFor="pass">Password</label>
 
-            <div className='relative'>
-              <input disabled={loading} value={password} onChange={(e) => setPassword(e.target.value)} className='w-full border-2 px-2 py-1 rounded-md border-Input-Border focus:border-Input-Focus outline-0' type={showPass ? "text" : "password"} id='pass' placeholder='Password' name='password' />
+            <div className='flex items-center justify-between'>
+              <label className='text-[13px] font-semibold text-Text-Primary' htmlFor="pass">Password</label>
+              <span className='text-[13px] text-Primary font-bold cursor-pointer'>Forgot Password?</span>
+            </div>
+
+            <div className='flex items-center justify-center w-full border rounded-md py-2 px-4 border-Input-Border focus:border-Input-Focus active:border-Input-Focus'>
+
+              <LockIcon className='text-Text-Secondary' size={20} />
+
+              <input
+                disabled={loading}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className='w-full border-0 px-3 py-1 rounded-md outline-0 text-sm'
+                type={showPass ? "text" : "password"}
+                id='pass'
+                placeholder='••••••••'
+                name='password'
+              />
 
               <button
                 type="button"
                 onClick={() => setShowPass(!showPass)}
-                className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer"
+                className="text-Text-Secondary cursor-pointer"
               >
-                {showPass ? <Eye size={16} /> : <EyeClosed size={16} />}
+                {showPass ? <Eye size={17} /> : <EyeClosed size={17} />}
               </button>
             </div>
 
           </div>
 
-
-          {message && (
-            <p className={`text-sm text-center w-full rounded-md p-2 mt-2 ${error ? 'text-Error bg-red-600/10' : 'text-Success bg-green-600/10'}`}>{message}</p>
-          )}
-
         </div>
 
-        <button className='bg-Btn-BG hover:bg-Primary-Hover text-white w-full text-center rounded-md p-2 cursor-pointer flex items-center justify-center gap-2' type='submit' disabled={loading}>
-          Register
+        <div className='flex items-center justify-start gap-3 px-1 my-4'>
+          <input type='checkbox' id='keepsigned' />
+          <label className='cursor-pointer text-[13px] font-[500] text-Text-Primary' name='keepsigned' htmlFor="keepsigned">Keep me signed in for 30 days</label>
+        </div>
+
+        {message && (
+          <p className={`text-sm text-center w-full rounded-md p-2 mt-2 mb-3 ${error ? 'text-Error bg-red-600/10' : 'text-Success bg-green-600/10'}`}>{message}</p>
+        )}
+
+        <button className='bg-Primary hover:bg-Primary-Hover transition-all duration-300 text-white w-full text-center rounded-md p-2 cursor-pointer flex items-center justify-center gap-1' type='submit' disabled={loading}>
+          Sign Up <ArrowRight size={17} />
           {loading ? <Loading className={`border-2 border-white size-4`} /> : null}
         </button>
 
       </form>
+
+      <div className='flex items-center justify-center w-full my-6'>
+        <div className='h-[1px] bg-Text-Secondary w-full'></div>
+        <span className='w-full text-[10px] font-bold text-Text-Secondary text-center'>OR CONTINUE WITH</span>
+        <div className='h-[1px] bg-Text-Secondary w-full'></div>
+      </div>
+
+      <div className='flex items-center justify-center gap-10 w-full'>
+        <button className='w-full flex items-center justify-center border border-black/10 rounded-md p-3 cursor-pointer text-sm gap-1'>
+          <GemIcon size={20} />
+          Google
+        </button>
+
+        <button className='w-full flex items-center justify-center border border-black/10 rounded-md p-3 cursor-pointer text-sm gap-1'>
+          <AppleIcon size={20} />
+          Apple
+        </button>
+      </div>
+
 
     </div>
   )
@@ -318,10 +405,10 @@ function LoginPage() {
         </div>
 
         {message && (
-          <p className={`text-sm text-center w-full rounded-md p-2 mt-2 ${error ? 'text-Error bg-red-600/10' : 'text-Success bg-green-600/10'}`}>{message}</p>
+          <p className={`text-sm text-center w-full rounded-md p-2 mt-2 mb-3 ${error ? 'text-Error bg-red-600/10' : 'text-Success bg-green-600/10'}`}>{message}</p>
         )}
 
-        <button className='bg-Primary text-white w-full text-center rounded-md p-2 cursor-pointer flex items-center justify-center gap-1' type='submit' disabled={loading}>
+        <button className='bg-Primary hover:bg-Primary-Hover transition-all duration-300 text-white w-full text-center rounded-md p-2 cursor-pointer flex items-center justify-center gap-1' type='submit' disabled={loading}>
           Sign In <ArrowRight size={17} />
           {loading ? <Loading className={`border-2 border-white size-4`} /> : null}
         </button>
@@ -335,12 +422,12 @@ function LoginPage() {
       </div>
 
       <div className='flex items-center justify-center gap-10 w-full'>
-        <button className='w-full flex items-center justify-center border border-black/10 rounded-md p-3 cursor-pointer text-sm gap-1'>
+        <button className='w-full flex items-center justify-center border border-black/10 hover:border-Btn-BG transition-all duration-300 rounded-md p-3 cursor-pointer text-sm gap-1'>
           <GemIcon size={20} />
           Google
         </button>
 
-        <button className='w-full flex items-center justify-center border border-black/10 rounded-md p-3 cursor-pointer text-sm gap-1'>
+        <button className='w-full flex items-center justify-center border border-black/10 hover:border-Btn-BG transition-all duration-300 rounded-md p-3 cursor-pointer text-sm gap-1'>
           <AppleIcon size={20} />
           Apple
         </button>
